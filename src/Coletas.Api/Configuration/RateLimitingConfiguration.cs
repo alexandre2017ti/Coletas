36a00ab2b0732039ac48c7803f01cd6ea6119a8b
Alexandre ortega <alexandre2017.ti@gmail.com>
2026-09-10 17:47:35 -0400

    style: aplicar formatacao da API

--- src/Coletas.Api/Configuration/RateLimitingConfiguration.cs
+++ src/Coletas.Api/Configuration/RateLimitingConfiguration.cs
@@ -17,6 +17,6 @@ public static class RateLimitingConfiguration
                 limiter.Window = TimeSpan.FromMinutes(1);
                 limiter.QueueLimit = 0;
             }));
-        
+
     }
 }
